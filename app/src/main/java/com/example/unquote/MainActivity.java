024813ca9db3f1e5a3aa94133a7d420813624b84
anja-se <anja-se@web.de>
2024-01-31 12:34:54 +0100

    initial commit

---
+++ app/src/main/java/com/example/unquote/MainActivity.java
@@ -0,0 +1,193 @@
+package com.example.unquote;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    int currentQuestionIndex;
+    int totalCorrect;
+    int totalQuestions;
+    ArrayList<Question> questions;
+
+    ImageView questionImageView;
+    TextView questionTextView;
+    TextView questionsRemainingTextView;
+    Button answer0Button;
+    Button answer1Button;
+    Button answer2Button;
+    Button answer3Button;
+    Button submitButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//Attempt to invoke virtual method 'void androidx.appcompat.app.ActionBar.setDisplayShowHomeEnabled(boolean)' on a null object reference
+        // TODO 2-G: Show app icon in ActionBar
+        //getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //getSupportActionBar().setLogo(R.drawable.ic_unquote_icon);
+        //getSupportActionBar().setDisplayUseLogoEnabled(true);
+        //getSupportActionBar().setElevation(0);
+
+        questionImageView = findViewById(R.id.iv_main_question_image);
+        questionTextView = findViewById(R.id.tv_main_question_title);
+        questionsRemainingTextView = findViewById(R.id.tv_main_questions_remaining_count);
+        answer0Button = findViewById(R.id.btn_main_answer_0);
+        answer1Button = findViewById(R.id.btn_main_answer_1);
+        answer2Button = findViewById(R.id.btn_main_answer_2);
+        answer3Button = findViewById(R.id.btn_main_answer_3);
+        submitButton = findViewById(R.id.btn_main_submit_answer);
+
+        answer0Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onAnswerSelected(0);
+            }
+        });
+        answer1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onAnswerSelected(1);
+            }
+        });
+        answer2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onAnswerSelected(2);
+            }
+        });
+        answer3Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onAnswerSelected(3);
+            }
+        });
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onAnswerSubmission();
+            }
+        });
+        startNewGame();
+    }
+
+void displayQuestion(Question question){
+        questionImageView.setImageResource(question.imageId);
+        questionTextView.setText(question.questionText);
+        answer0Button.setText(question.answer0);
+        answer1Button.setText(question.answer1);
+        answer2Button.setText(question.answer2);
+        answer3Button.setText(question.answer3);
+}
+    void displayQuestionsRemaining(int questionRemaining) {
+        questionsRemainingTextView.setText(String.valueOf(questionRemaining));
+    }
+
+    void onAnswerSelected(int answerSelected) {
+        Question currentQuestion = getCurrentQuestion();
+        currentQuestion.playerAnswer = answerSelected;
+        Button[] answerButtons = {answer0Button, answer1Button, answer2Button, answer3Button};
+        String[] allAnswers = {currentQuestion.answer0, currentQuestion.answer1, currentQuestion.answer2, currentQuestion.answer3};
+
+        for (int i = 0; i < 4; i++) {
+            if (i == answerSelected) {
+                answerButtons[i].setText("✔ " + allAnswers[i]);
+            } else {
+                answerButtons[i].setText(allAnswers[i]);
+            }
+        }
+    }
+
+    void onAnswerSubmission() {
+        Question currentQuestion = getCurrentQuestion();
+
+        if (currentQuestion.isCorrect()) {
+            totalCorrect ++;
+        }
+        questions.remove(currentQuestion);
+
+        displayQuestionsRemaining(questions.size());
+
+        if (questions.size() == 0) {
+            String gameOverMessage = getGameOverMessage(totalCorrect, totalQuestions);
+
+            // TODO 5-D: Show a popup instead
+            AlertDialog.Builder gameOverDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+            gameOverDialogBuilder.setCancelable(false);
+            gameOverDialogBuilder.setTitle("Game Over!");
+            gameOverDialogBuilder.setMessage(gameOverMessage);
+            gameOverDialogBuilder.setPositiveButton("Play Again!", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            startNewGame();
+                        }
+                    }
+            );
+            gameOverDialogBuilder.create().show();
+        } else {
+            chooseNewQuestion();
+            displayQuestion(getCurrentQuestion());
+        }
+    }
+
+    void startNewGame() {
+        questions = new ArrayList<>();
+        Question question0 = new Question(R.drawable.img_quote_0, "Pretty good advice, and perhaps a scientist did say it... Who actually did?", "Albert Einstein", "Isaac Newton", "Rita Mae Brown", "Rosalind Franklin", 2);
+        Question question1 = new Question(R.drawable.img_quote_1, "Was honest Abe honestly quoted? Who authored this pithy bit of wisdom?", "Edward Stieglitz", "Maya Angelou", "Abraham Lincoln", "Ralph Waldo Emerson", 0);
+        Question question2 = new Question(R.drawable.img_quote_2, "Easy advice to read, difficult advice to follow — who actually said it?", "Martin Luther King Jr.", "Mother Teresa", "Fred Rogers", "Oprah Winfrey", 1);
+        Question question3 = new Question(R.drawable.img_quote_3, "Insanely inspiring, insanely incorrect (maybe). Who is the true source of this inspiration?", "Nelson Mandela", "Harriet Tubman", "Mahatma Gandhi", "Nicholas Klein", 3);
+        Question question4 = new Question(R.drawable.img_quote_4, "A peace worth striving for — who actually reminded us of this?", "Malala Yousafzai", "Martin Luther King Jr.", "Liu Xiaobo", "Dalai Lama", 1);
+        Question question5 = new Question(R.drawable.img_quote_5, "Unfortunately, true — but did Marilyn Monroe convey it or did someone else?", "Laurel Thatcher Ulrich", "Eleanor Roosevelt", "Marilyn Monroe", "Queen Victoria", 0);
+
+        questions.add(question0);
+        questions.add(question1);
+        questions.add(question2);
+        questions.add(question3);
+        questions.add(question4);
+        questions.add(question5);
+
+        totalCorrect = 0;
+        totalQuestions = questions.size();
+
+        Question firstQuestion = chooseNewQuestion();
+        displayQuestionsRemaining(questions.size());
+        displayQuestion(firstQuestion);
+    }
+
+    Question chooseNewQuestion() {
+        int newQuestionIndex = generateRandomNumber(questions.size());
+        currentQuestionIndex = newQuestionIndex;
+        return questions.get(currentQuestionIndex);
+    }
+
+    int generateRandomNumber(int max) {
+        double randomNumber = Math.random();
+        double result = max * randomNumber;
+        return (int) result;
+    }
+
+    Question getCurrentQuestion() {
+        Question currentQuestion = questions.get(currentQuestionIndex);
+        return currentQuestion;
+    }
+
+    String getGameOverMessage(int totalCorrect, int totalQuestions) {
+        if (totalCorrect == totalQuestions) {
+            return "You got all " + totalQuestions + " right! You won!";
+        } else {
+            return "You got " + totalCorrect + " right out of " + totalQuestions + ". Better luck next time!";
+        }
+    }
+}
